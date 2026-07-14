0b48580732e9aeb18ed28de1effde63113438316
Syed Nazima <vu.241fa04306@gmail.com>
2026-07-14 10:45:15 +0530

    Time: 4 ms (7.17%), Space: 19.1 MB (84.12%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        total = 0
-        for i in range(len(s) - 1):
-            total += abs(ord(s[i]) - ord(s[i + 1]))
-        return total
+        ans = 0
+        for i in range(len(s)-1):
+            ans = ans + abs(ord(s[i]) - ord(s[i+1]))
+        return ans
         
