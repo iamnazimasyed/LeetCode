c7ff8be085b44c2ab4c2d0313fd632fd4333031e
Syed Nazima <vu.241fa04306@gmail.com>
2026-08-31 22:46:25 +0530

    Time: 0 ms (100%), Space: 19.2 MB (57.14%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -1,12 +1,13 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        x = 0
-        for i in range(len(operations)):
-            if operations[i] == "X--" or operations[i] == "--X":
-                x = x - 1
+        X = 0
+        for i in operations:
+            if i == "--X" or i == "X--":
+                X = X - 1
             else:
-                x = x + 1
-        return x
+                X = X + 1
+        return X
+                
         
     
         
